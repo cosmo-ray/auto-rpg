4757cf6e13af85637445abbdf6ef09b9e9c2a88b
Matthias Gatto <uso.cosmo.ray@gmail.com>
2024-01-04 13:50:04 +0100

    attack mouvement, butno weapon ?
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

--- start.js
+++ start.js
@@ -2,6 +2,20 @@
 const good_orig_pos = [1, 1]
 const bad_orig_pos = [1, 3]
 
+const weapons = {
+    "longsword": {
+	"len": 6,
+	"pos": 13
+    },
+    "bow": {
+	"len": 13,
+	"pos": 17
+    },
+    "spear": {
+	"len": 8,
+	"pos": 5
+    }
+}
 const good_sword_swing = 13
 const bad_sword_swing = 15
 const sword_swing_len = 6
@@ -21,18 +35,76 @@ const arc_shoot_len = 13
  * oversize sprite: Universal-LPC-spritesheet/spritesheets/weapons/oversize/right hand/male/
  */
 
+let turn_countdown = 0
+let attacker = []
+let atk_time = 4000000
+let atk_cooldown = 100000
+
 function jrpg_auto_action(wid, eve)
 {
-    print("action !\n")
+    let all_units = yeGet(wid, "units")
+    let turn_timer = ywidGetTurnTimer()
+
+    if (attacker.length > 0) {
+	attacker = attacker.filter(function (a) {return a[2] < (atk_time + atk_cooldown)})
+
+	for (a of attacker) {
+	    let h = wid.get("handlers").get(a[0]).get(a[1])
+	    let w = all_units.get(a[0]).get(a[1]).get("weapon")
+	    let w_info = weapons[yeGetStringAt(w, "name")]
+	    print("attacker:", a)
+	    //yePrint(w)
+	    yePrint(h)
+	    if (a[2] > atk_time) {
+		ylpcsHandlerSetOrigXY(h, 1, good_orig_pos[1] + (2 * a[0]))
+	    } else {
+		ylpcsHandlerSetOrigXY(h,
+				      Math.abs(a[2] * w_info["len"] / atk_time),
+				      w_info["pos"] + (2 * a[0])
+				     )
+	    }
+	    ylpcsHandlerRefresh(h)
+	    a[2] += turn_timer
+	}
+	return
+    }
+
+    turn_countdown += turn_timer
+
+    if (turn_countdown > 400000) {
+	turn_countdown = 0
+	all_units.forEach(function (units, i) {
+	    units.forEach(function (u, j) {
+		yePrint(u.get("name"))
+		print("j: ", j, "i: ", i)
+		yePrint(u.get("atk-load"))
+		print("agy:", u.get("stats").geti("agility"))
+		print("heady:", u.get("armor")?.geti("heavy"))
+		print("maniability:", u.get("weapon").geti("maniability"))
+		let to_add = 10 + u.get("stats").geti("agility") -
+		    u.get("weapon").geti("maniability") -
+		    u.get("armor")?.geti("heavy")
+		if (to_add < 1)
+		    to_add = 1
+		u.addAt("atk-load", to_add)
+		if (u.geti("atk-load") >= 100) {
+		    u.addAt("atk-load", -100)
+		    attacker.push([i, j, 0])
+		}
+		yePrint(u.get("atk-load"))
+	    })
+	})
+    }
 }
 
 function jrpg_auto_init(wid, map_str)
 {
     yeConvert(wid, YHASH)
+    ywSetTurnLengthOverwrite(-1)
     yeCreateString("canvas", wid, "<type>")
     yeCreateString("rgba: 255 255 255 255", wid, "background")
     yeCreateFunction(jrpg_auto_action, wid, "action")
-    var units = yeGet(wid, "units")
+    let units = yeGet(wid, "units")
 
     if (units == null) {
 	units = ygFileToEnt(YJSON, "./units.json")
@@ -48,6 +120,9 @@ function jrpg_auto_init(wid, map_str)
     let bad_guy = yeGet(units, 1)
     let good_handlers = yeCreateArray(wid, "good handler")
     let bad_handlers = yeCreateArray(wid, "bad handler")
+    let handlers = yeCreateArray(wid, "handlers")
+    yePushBack(handlers, good_handlers)
+    yePushBack(handlers, bad_handlers)
 
     const good_x_threshold = 40
     const bad_x_threshold = 100
@@ -55,9 +130,10 @@ function jrpg_auto_init(wid, map_str)
     yePrint(good_guy)
     for (let i = 0; i < 3; ++i) {
 	let guy = yeGet(good_guy, i)
-	yeConvert(guy, YHASH)
 
 	if (guy) {
+	    yeConvert(guy, YHASH)
+	    guy.setAt(0, "atk-load")
 	    let guy_h = ylpcsCreateHandler(guy, wid)
 	    ylpcsHandlerSetOrigXY(guy_h, good_orig_pos[0], good_orig_pos[1])
 	    ylpcsHandlerRefresh(guy_h)
@@ -69,9 +145,10 @@ function jrpg_auto_init(wid, map_str)
 
     for (let i = 0; i < 3; ++i) {
 	let guy = yeGet(good_guy, i + 3)
-	yeConvert(guy, YHASH)
 
 	if (guy) {
+	    yeConvert(guy, YHASH)
+	    guy.setAt(0, "atk-load")
 	    let guy_h = ylpcsCreateHandler(guy, wid)
 	    ylpcsHandlerSetOrigXY(guy_h, good_orig_pos[0], good_orig_pos[1])
 	    ylpcsHandlerRefresh(guy_h)
@@ -83,9 +160,10 @@ function jrpg_auto_init(wid, map_str)
 
     for (let i = 0; i < 3; ++i) {
 	let guy = yeGet(bad_guy, i)
-	yeConvert(guy, YHASH)
 
 	if (guy) {
+	    yeConvert(guy, YHASH)
+	    guy.setAt(0, "atk-load")
 	    let guy_h = ylpcsCreateHandler(guy, wid)
 	    ylpcsHandlerSetOrigXY(guy_h, bad_orig_pos[0], bad_orig_pos[1])
 	    ylpcsHandlerRefresh(guy_h)
@@ -97,9 +175,10 @@ function jrpg_auto_init(wid, map_str)
 
     for (let i = 0; i < 3; ++i) {
 	let guy = yeGet(bad_guy, i + 3)
-	yeConvert(guy, YHASH)
 
 	if (guy) {
+	    yeConvert(guy, YHASH)
+	    guy.set_at(0, "atk-load")
 	    let guy_h = ylpcsCreateHandler(guy, wid)
 	    ylpcsHandlerSetOrigXY(guy_h, bad_orig_pos[0], bad_orig_pos[1])
 	    ylpcsHandlerRefresh(guy_h)
